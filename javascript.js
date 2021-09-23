f573deff548e08a2737387404997d503ebc6d842
bumsausende <feinen.kerl@gmail.com>
2021-09-23 09:45:30 +0200

    bookmark icon and JS events

--- javascript.js
+++ javascript.js
@@ -14,4 +14,43 @@ const hrefAttribute = link.getAttribute("href");
 const nextPage = document.querySelector(hrefAttribute);
 });
 
+//show Answer
 
+
+
+//Submit new question:
+console.clear();
+const form = document.querySelector("form");
+const newInputList = [];
+
+
+form.addEventListener("submit", (event) => {
+
+let newInput = {
+    questionCreate: form.elements.questionCreate.value,
+    questionAnswer: form.elements.questionAnswer.value,
+    questionTags: form.elements.questionTags.value,
+};
+    console.log(newInput);
+    event.preventDefault();
+    form.reset();
+    newInputList.push(newInput);
+});
+
+
+/*Bookmarked marked by click */
+
+const unmarkedBookmarkList = document.querySelectorAll(".bookmarkIcon");
+unmarkedBookmarkList.forEach((bookmark)=>{
+    bookmark.addEventListener("click", ()=>{
+        bookmark.classList.toggle("bookmarkedIcon");
+    })
+})
+ const buttonAnswerList = document.querySelectorAll(".buttonAnswer");
+  buttonAnswerList.forEach((buttonAnswer)=>{
+    const answerButtonShow = answerButton.querySelector(".answerbutton");
+    const answer = answerButton.querySelector(".answer")
+    answerButton.addEventListener ("click", ()=>{
+        answer.classList.toggle("show");
+    });
+  });
